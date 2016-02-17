c4c9e11f1030378f069527d91c4749329078f36a
Roque Rueda <skaroque@gmail.com>
2016-02-17 01:02:30 -0600

    Adding single fragment activity.

---
+++ BlackList/app/src/main/java/rueda/roque/android/com/blacklist/SingleFragmentActivity.java
@@ -0,0 +1,11 @@
+package rueda.roque.android.com.blacklist;
+
+/**
+ * TODO: Class description.
+ *
+ * @author root
+ * @version 1.0
+ * @since 2/17/16
+ */
+public class SingleFragmentActivity {
+}
