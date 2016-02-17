a9a39af17a6705b766541188546c55a4b334a9f3
Roque Rueda <skaroque@gmail.com>
2016-02-16 19:09:09 -0600

    Initial commit.
    Start of the project.

---
+++ BlackList/app/src/main/java/rueda/roque/android/com/blacklist/BlackListActivity.java
@@ -0,0 +1,13 @@
+package rueda.roque.android.com.blacklist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BlackListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_black_list);
+    }
+}
